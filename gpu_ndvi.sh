3c7afef409300ece37edf9635bf3a07d5ea6aa83
hchen0628 <143530699+hchen0628@users.noreply.github.com>
2024-04-10 03:34:09 -0500

    Update gpu_ndvi.sh

--- gpu_ndvi.sh
+++ gpu_ndvi.sh
@@ -3,13 +3,13 @@
 #SBATCH --job-name=gpu_ndvi
 #SBATCH --output=gpu_ndvi.out
 #SBATCH --error=gpu_ndvi.err
-#SBATCH --nodes=1  # Using 1 GPU node
-#SBATCH --ntasks-per-node=1  # 1 CPU node to drive the GPU
-#SBATCH --partition=gpu  # Using the GPU partition on Midway3
-#SBATCH --gres=gpu:1  # Requesting only one GPU
-#SBATCH --account=macs30123
-#SBATCH --mem-per-cpu=30G  # Restrict CPU memory usage by 30G
+#SBATCH --nodes=1              # Request 1 GPU node
+#SBATCH --ntasks-per-node=1    # Request 1 task per GPU node
+#SBATCH --partition=gpu        # Specify the GPU partition
+#SBATCH --gres=gpu:1           # Request 1 GPU
+#SBATCH --account=macs30123    # Specify the account for job charging
+#SBATCH --mem-per-cpu=30G      # Limit the memory usage per CPU
 
-module load python/3.6
+module load python/3.8.0
 
-python3 "123 3a.py"
+python3 "/home/hchen0628/MACS30123-A1/123_3a.py"
